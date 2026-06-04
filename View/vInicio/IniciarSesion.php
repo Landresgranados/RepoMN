413f79c2f83160f79419227040bc3300858bfdd9
Luis Andrés <luisandresgg06@gmail.com>
2026-06-03 19:12:41 -0600

    Funciones e imports

--- View/vInicio/IniciarSesion.php
+++ View/vInicio/IniciarSesion.php
@@ -1,12 +1,13 @@
+<?php
+    include_once "../LayoutExterno.php";
+?>
+
 <!doctype html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <title>Proyecto Web MN</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="../css/main.css" />
-</head>
+<?php
+    ImportCSS();
+?>
 
 <body>
     <div class="container d-flex align-items-center justify-content-center min-vh-100">
@@ -43,8 +44,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap JS -->
 </body>
 
 </html>
